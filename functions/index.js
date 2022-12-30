884cdcc49df06ff51ade34ae16b2350a5fb6ba59
Annnyan <novikovaannna@gmail.com>
2022-12-30 22:26:38 +0200

    Create telegram bot

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,21 @@
 const functions = require("firebase-functions");
+const {Telegraf} = require("telegraf");
 
+let config = require ("./env.json");
+if (Object.keys(functions.config()).length) {
+	config = functions.config();
+}
+
+const bot = new Telegraf(config.service.bot_token);
+
+bot.start((ctx) => {
+    ctx.reply("Hello!");
+});
+
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
